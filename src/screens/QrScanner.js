7aafb1ec3a33af2d7239fc81e4590879bf594fa4
Logeshdas <logeshwaranthulasidass@gmail.com>
2020-02-14 16:13:36 +0530

    Added QRscanner.

--- src/screens/QrScanner.js
+++ src/screens/QrScanner.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, Text, StyleSheet,Alert} from 'react-native';
+import {QRScannerRectView} from 'react-native-qrcode-scanner-view';
 
-function QrScanner() {
-  return (
-    <View>
-      <Text>QrScanner</Text>
-    </View>
+import {RNCamera} from 'react-native-camera';
+
+class CodeScan extends Component {
+
+  renderTitleBar = () => <Text>Title</Text>;
+
+  renderMenu = () => (
+    <Text style={{color: 'white', textAlign: 'center', padding: 16}}>Menu</Text>
   );
+  barcodeReceived = event => {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <RNCamera
+          style={styles.preview}
+          onBarCodeRead={this.barcodeReceived}
+          renderFooterView={this.renderMenu}
+          scanBarAnimateReverse>
+          <QRScannerRectView
+            renderHeaderView={this.renderTitleBar}
+            hintText="scan"
+          />
+        </RNCamera>
+      </SafeAreaView>
+    );
+  }
 }
 
-export default QrScanner;
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    // padding: UI.scaleSize(16),
+  },
+  preview: {
+    flex: 1,
+  },
+});
+export default CodeScan;
